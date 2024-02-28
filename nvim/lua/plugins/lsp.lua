588da9db75eead64d2d9ed31da5d19b0b1bfd1cd
Isaac Holston <32341824+iholston@users.noreply.github.com>
2024-02-28 10:12:53 -0700

    refac(lsp): add none-ls

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -40,6 +40,22 @@ return {
         end
     },
 
+    -- Null-ls
+    {
+        "nvimtools/none-ls.nvim",
+        config = function()
+            local null_ls = require("null-ls")
+            null_ls.setup({
+                sources = {
+                    null_ls.builtins.formatting.stylua,
+                    null_ls.builtins.formatting.black,
+                    null_ls.builtins.formatting.isort,
+                },
+            })
+            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+        end,
+    },
+
     -- Nvim-LspConfig
     {
         "neovim/nvim-lspconfig",
