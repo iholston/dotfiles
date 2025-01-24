40f09887b439ab1f95fb0980be5d83dc8c81a7ba
Isaac Holston <32341824+iholston@users.noreply.github.com>
2025-01-24 08:42:47 -0500

    refactor(wez): update leader key and misc changes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,29 +1,24 @@
-local wezterm = require("wezterm")
-local act = wezterm.action
-local mux = wezterm.mux
+local wezterm   = require("wezterm")
+local act       = wezterm.action
+local mux       = wezterm.mux
+local config    = {}
 
-local config = {}
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
--- wezterm.on("gui-startup", function(cmd)
---     local tab, pane, window = mux.spawn_window(cmd or {})
---     window:gui_window():maximize()
--- end)
-
--- Settings
-config.color_scheme = "Dracula (base16)"
+-- Basic Settings
+config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font("JetBrains Mono")
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.99
+config.window_background_opacity = 1
 config.window_close_confirmation = "NeverPrompt"
 config.scrollback_lines = 3000
 config.default_workspace = "home"
 config.default_prog = { "C:\\Program Files\\PowerShell\\7\\pwsh.exe" }
 
--- Keys
-config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+-- KeyBinds
+config.leader = { key = "w", mods = "ALT", timeout_milliseconds = 1000 }
 config.keys = {
 
     -- Send C-a when pressed 2x
@@ -41,38 +36,20 @@ config.keys = {
     -- Pane 
     { key = "s",          mods = "LEADER",      action = act.SplitVertical { domain = "CurrentPaneDomain" } },
     { key = "v",          mods = "LEADER",      action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
-    { key = "q",          mods = "LEADER",      action = act.CloseCurrentPane { confirm = true } },
+    { key = "q",          mods = "LEADER",      action = act.CloseCurrentPane { confirm = false } },
     { key = "z",          mods = "LEADER",      action = act.TogglePaneZoomState },
     { key = "h",          mods = "LEADER",      action = act.ActivatePaneDirection("Left") },
     { key = "j",          mods = "LEADER",      action = act.ActivatePaneDirection("Down") },
     { key = "k",          mods = "LEADER",      action = act.ActivatePaneDirection("Up") },
     { key = "l",          mods = "LEADER",      action = act.ActivatePaneDirection("Right") },
 
-    -- Workspaces
-    { key = "w",          mods = "LEADER",      action = act.ShowLauncherArgs { flags = "FUZZY|WORKSPACES" } },
-    { key = "D",          mods = "LEADER",      action = act.SwitchToWorkspace {
-            name = "Dotfiles",
-            spawn = { cwd = wezterm.home_dir .. "/GitHub/dotfiles" },
-        },
-    },
-    { key = "L",          mods = "LEADER",      action = act.SwitchToWorkspace {
-            name = 'LoL Bot',
-            spawn = { cwd = wezterm.home_dir .. "/GitHub/lol-bot" },
-        },
-    },
-    { key = "A",          mods = "LEADER",      action = act.SwitchToWorkspace {
-            name = 'LoL Accept',
-            spawn = { cwd = wezterm.home_dir .. "/GitHub/lol-accept" },
-        },
-    },
+    -- Move through scrollback with JK
+    { key = "k",          mods = "CTRL|SHIFT",  action = act.ScrollByPage(-0.2) },
+    { key = "j",          mods = "CTRL|SHIFT",  action = act.ScrollByPage(0.2) },
 
     -- Tab
     { key = "t",          mods = "LEADER",      action = act.SpawnTab("CurrentPaneDomain") },
     { key = "n",          mods = "LEADER",      action = act.ShowTabNavigator },
-
-    -- Move through scrollback with JK
-    { key = "k",          mods = "CTRL|SHIFT",  action = act.ScrollByPage(-0.2) },
-    { key = "j",          mods = "CTRL|SHIFT",  action = act.ScrollByPage(0.2) },
 
     -- Rename Tab
     {
@@ -92,23 +69,6 @@ config.keys = {
         }
     },
 }
-
--- Override copy mode keys https://wezfurlong.org/wezterm/config/lua/wezterm.gui/default_key_tables.html
-local copy_mode = nil
-if wezterm.gui then
-    copy_mode = wezterm.gui.default_key_tables().copy_mode
-    table.insert(
-    copy_mode,
-    { key = "_", mods = "SHIFT", action = act.CopyMode "MoveToStartOfLineContent" }
-    )
-    table.insert(
-    copy_mode,
-    { key="y",   mods="NONE",    action=wezterm.action{ Multiple = {
-        wezterm.action { CopyTo="ClipboardAndPrimarySelection" },
-        wezterm.action { CopyMode="Close" }
-    }}}
-    )
-end
 
 -- Resize panes/move tabs with hjkl
 config.key_tables = {
@@ -132,14 +92,5 @@ config.key_tables = {
     },
     copy_mode = copy_mode,
 }
-
--- Leader number to jump to tab
-for i = 1, 9 do
-  table.insert(config.keys, {
-    key = tostring(i),
-    mods = "LEADER",
-    action = act.ActivateTab(i - 1)
-  })
-end
 
 return config
